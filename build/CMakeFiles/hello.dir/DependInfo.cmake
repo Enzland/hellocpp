62b9ea9a01a37894940fee687a0231b49febc8db
Peter Froehlich <pefroeli@gmail.com>
2022-05-22 18:19:55 +0200

    Downgraded cmake version

--- build/CMakeFiles/hello.dir/DependInfo.cmake
+++ build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -1,15 +1,17 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/peter/src/hellocpp/main.cpp" "/home/peter/src/hellocpp/build/CMakeFiles/hello.dir/main.cpp.o"
+  "/home/peter/src/hellocpp/src/Blah.cpp" "/home/peter/src/hellocpp/build/CMakeFiles/hello.dir/src/Blah.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/peter/src/hellocpp/main.cpp" "CMakeFiles/hello.dir/main.cpp.o" "gcc" "CMakeFiles/hello.dir/main.cpp.o.d"
-  "/home/peter/src/hellocpp/src/Blah.cpp" "CMakeFiles/hello.dir/src/Blah.cpp.o" "gcc" "CMakeFiles/hello.dir/src/Blah.cpp.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
   )
 
 # Targets to which this target links.
